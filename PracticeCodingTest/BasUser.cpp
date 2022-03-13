4a79c2a426d4b954a526ed201e798c6952e98b87
OneBarrack <wnsdd1@gmail.com>
2022-03-14 05:15:44 +0900

    * Clear - Programmers Lv3. Bad User

---
+++ PracticeCodingTest/BasUser.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv3 불량 사용자
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<string> user_id, vector<string> banned_id)
+{
+    set<set<string>> totalBanLists;
+
+    for ( int banIndex = 0; banIndex < banned_id.size(); ++banIndex )
+    {
+        string bannedId = banned_id[banIndex];
+        set<set<string>> curBanLists;
+
+        for ( int userIndex = 0; userIndex < user_id.size(); ++userIndex )
+        {
+            string userId = user_id[userIndex];
+            if ( userId.size() != bannedId.size() )
+                continue;
+
+            bool bFindMatched = true;
+            for ( int i = 0; i < userId.size(); ++i )
+            {
+                if ( bannedId[i] != userId[i] && bannedId[i] != '*' )
+                {
+                    bFindMatched = false;
+                    break;
+                }
+            }
+
+            if ( bFindMatched )
+            {                
+                if ( totalBanLists.empty() )
+                {
+                    set<string> banList;
+                    banList.insert(userId);
+                    curBanLists.insert(banList);
+                }
+                else
+                {
+                    for ( set<string> banList : totalBanLists )
+                    {
+                        banList.insert(userId);
+                        if ( banList.size() > banIndex )
+                        {
+                            curBanLists.insert(banList);
+                        }
+                    }
+                }
+            }
+        }
+
+        totalBanLists = curBanLists;
+    }
+
+    return totalBanLists.size();
+}
+
+int main()
+{
+    vector<string> user_id = { "frodo", "fradi", "crodo", "abc123", "frodoc" };
+    //vector<string> banned_id = { "fr*d*", "abc1**" };
+    //vector<string> banned_id = { "*rodo", "*rodo", "******" };
+    vector<string> banned_id = { "fr*d*", "*rodo", "******", "******" };
+    cout << solution(user_id, banned_id) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/ExpectedTournamentTree.cpp
+++ PracticeCodingTest/ExpectedTournamentTree.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv2 예상 대진표
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-
-using namespace std;
-
-bool IsEven(int n)
-{
-    return (n % 2 == 0) ? true : false;
-}
-
-int solution(int n, int a, int b)
-{
-    int answer = 1;
-
-    while ( true )
-    {
-        if ( !IsEven(a) ) ++a;
-        if ( !IsEven(b) ) ++b;
-
-        if ( a == b )
-            break;
-
-        a /= 2;
-        b /= 2;
-        ++answer;
-    }
-
-    return answer;
-}
-
-int main()
-{
-    int n= 8;
-    int a = 4;
-    int b = 7;
-    cout << solution(n, a, b) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv2 예상 대진표
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//
+//using namespace std;
+//
+//bool IsEven(int n)
+//{
+//    return (n % 2 == 0) ? true : false;
+//}
+//
+//int solution(int n, int a, int b)
+//{
+//    int answer = 1;
+//
+//    while ( true )
+//    {
+//        if ( !IsEven(a) ) ++a;
+//        if ( !IsEven(b) ) ++b;
+//
+//        if ( a == b )
+//            break;
+//
+//        a /= 2;
+//        b /= 2;
+//        ++answer;
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    int n= 8;
+//    int a = 4;
+//    int b = 7;
+//    cout << solution(n, a, b) << endl;
+//
+//    return 0;
+//}
