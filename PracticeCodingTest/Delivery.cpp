1ac4fe4d14d260308c7e95ebaeda1dc4ee0354af
OneBarrack <wnsdd1@gmail.com>
2022-03-20 04:31:59 +0900

    * Clear - Programmers Lv2. Delivery

---
+++ PracticeCodingTest/Delivery.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv2 배달
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+void DFS(bool* visit, vector<vector<pair<int, int>>>& townNodes, int town, int K, set<int>& result)
+{
+    result.insert(town);
+    visit[town] = true;
+
+    for ( pair<int, int>& townNode : townNodes[town] )
+    {
+        if ( visit[townNode.first] )
+            continue;
+
+        if ( townNode.second <= K )
+        {
+            DFS(visit, townNodes, townNode.first, K - townNode.second, result);
+        }
+    }
+
+    visit[town] = false;
+}
+
+int solution(int N, vector<vector<int>> road, int K)
+{
+    set<int> result;
+    vector<vector<pair<int, int>>> townNodes(51);
+    bool visit[51] = { false, };
+
+    for ( vector<int> node : road )
+    {
+        townNodes[node[0]].emplace_back(make_pair(node[1], node[2]));
+        townNodes[node[1]].emplace_back(make_pair(node[0], node[2]));
+    }
+
+    DFS(visit, townNodes, 1, K, result);
+
+    return result.size();
+}
+
+int main()
+{
+    int N = 6;
+    //vector<vector<int>> road = { {1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2} };
+    vector<vector<int>> road = { {1,2,1} ,{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1} };
+    int K = 4;
+    
+    cout << solution(N, road, K) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/WordConversion.cpp
+++ PracticeCodingTest/WordConversion.cpp
@@ -1,101 +1,101 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv3 단어 변환
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-bool IsPossibleConversion(string word1, string word2)
-{
-    int matchedCount = 0;
-    for ( int i = 0; i < word1.size(); ++i )
-    {
-        if ( word1[i] == word2[i] )
-            ++matchedCount;
-    }
-
-    return (matchedCount == word1.size() - 1) ? true : false;
-}
-
-int solution(string begin, string target, vector<string> words)
-{
-    int answer = 0;
-
-    unordered_map<string, vector<string>> wordMap;
-    unordered_map<string, bool> visit;
-
-    for ( int i = 0; i < words.size(); ++i )
-    {
-        string word = words[i];
-        if ( IsPossibleConversion(begin, word) )
-        {
-            wordMap[begin].emplace_back(word);
-        }        
-    }
-
-    for ( int i = 0; i < words.size(); ++i )
-    {
-        string word1 = words[i];
-
-        for ( int j = 0; j < words.size(); ++j )
-        {
-            string word2 = words[j];
-            if ( word1 == word2 )
-                continue;
-
-            if ( IsPossibleConversion(word1, word2) )
-            {
-                wordMap[word1].emplace_back(word2);
-            }
-        }
-    }
-    
-    queue<pair<string, int>> wordQueue;
-    wordQueue.push(make_pair(begin, 0));
-
-    while ( !wordQueue.empty() )
-    {
-        string curWord = wordQueue.front().first;
-        int convertCount = wordQueue.front().second;
-        wordQueue.pop();
-
-        if ( curWord == target )
-        {
-            answer = convertCount;
-            break;
-        }            
-
-        visit[curWord] = true;
-
-        for ( string nextWord : wordMap[curWord] )
-        {
-            if ( !visit[nextWord] )
-            {
-                wordQueue.push(make_pair(nextWord, convertCount + 1));
-            }
-        }
-    }
-
-    return answer;
-}
-
-int main()
-{
-    string begin = "hit";
-    string target = "cog";
-    vector<string> words = { "hot", "dot", "dog", "lot", "log" };
-
-    cout << solution(begin, target, words) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv3 단어 변환
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//bool IsPossibleConversion(string word1, string word2)
+//{
+//    int matchedCount = 0;
+//    for ( int i = 0; i < word1.size(); ++i )
+//    {
+//        if ( word1[i] == word2[i] )
+//            ++matchedCount;
+//    }
+//
+//    return (matchedCount == word1.size() - 1) ? true : false;
+//}
+//
+//int solution(string begin, string target, vector<string> words)
+//{
+//    int answer = 0;
+//
+//    unordered_map<string, vector<string>> wordMap;
+//    unordered_map<string, bool> visit;
+//
+//    for ( int i = 0; i < words.size(); ++i )
+//    {
+//        string word = words[i];
+//        if ( IsPossibleConversion(begin, word) )
+//        {
+//            wordMap[begin].emplace_back(word);
+//        }        
+//    }
+//
+//    for ( int i = 0; i < words.size(); ++i )
+//    {
+//        string word1 = words[i];
+//
+//        for ( int j = 0; j < words.size(); ++j )
+//        {
+//            string word2 = words[j];
+//            if ( word1 == word2 )
+//                continue;
+//
+//            if ( IsPossibleConversion(word1, word2) )
+//            {
+//                wordMap[word1].emplace_back(word2);
+//            }
+//        }
+//    }
+//    
+//    queue<pair<string, int>> wordQueue;
+//    wordQueue.push(make_pair(begin, 0));
+//
+//    while ( !wordQueue.empty() )
+//    {
+//        string curWord = wordQueue.front().first;
+//        int convertCount = wordQueue.front().second;
+//        wordQueue.pop();
+//
+//        if ( curWord == target )
+//        {
+//            answer = convertCount;
+//            break;
+//        }            
+//
+//        visit[curWord] = true;
+//
+//        for ( string nextWord : wordMap[curWord] )
+//        {
+//            if ( !visit[nextWord] )
+//            {
+//                wordQueue.push(make_pair(nextWord, convertCount + 1));
+//            }
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    string begin = "hit";
+//    string target = "cog";
+//    vector<string> words = { "hot", "dot", "dog", "lot", "log" };
+//    
+//    cout << solution(begin, target, words) << endl;
+//
+//    return 0;
+//}
