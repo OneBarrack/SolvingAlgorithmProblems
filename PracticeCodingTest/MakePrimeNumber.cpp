14c2f7ff73dbbe8c57cba5b066ec07c8e0ff59fb
OneBarrack <wnsdd1@gmail.com>
2022-03-22 21:58:49 +0900

    * Clear - Programmers Lv1. Make prime number

---
+++ PracticeCodingTest/MakePrimeNumber.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv1 소수 만들기
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+bool IsPrimeNum(int n)
+{
+    for ( int i = 2; i < n / 2; ++i )
+    {
+        if ( n % i == 0 )
+            return false;
+    }
+    return true;
+}
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+
+    for ( int i = 0; i < nums.size() - 2; ++i )
+        for ( int j = i + 1; j < nums.size() - 1; ++j )
+            for ( int k = j + 1; k < nums.size(); ++k )
+                if ( IsPrimeNum(nums[i] + nums[j] + nums[k]) )
+                    ++answer;
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> nums = { 1, 2, 3, 4 };
+    cout << endl << solution(nums) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/NoMoreThan2DifferentBits.cpp
+++ PracticeCodingTest/NoMoreThan2DifferentBits.cpp
@@ -1,84 +1,84 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv2 2개 이하로 다른 비트
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-vector<long long> solution(vector<long long> numbers)
-{
-    vector<long long> answer;
-    queue<int> bitQueue;
-
-    for ( long long num : numbers )
-    {
-        long long result = 0;
-
-        if ( num % 2 == 0 )
-        {
-            result = num + 1;
-        }
-        else
-        {
-            int order = 1;
-            int zeroPosOrder = 0;
-            while ( true )
-            {
-                int remainder = num % 2;
-                if ( remainder == 0 && zeroPosOrder == 0 )
-                {
-                    zeroPosOrder = order;
-                }
-                bitQueue.push(remainder);
-
-                if ( num == 0 )
-                    break;
-
-                num /= 2;
-                ++order;
-            }
-
-            order = 1;
-            long long square = 1;
-            while ( !bitQueue.empty() )
-            {
-                int n = bitQueue.front();
-                bitQueue.pop();
-
-                if ( order == zeroPosOrder - 1 || order == zeroPosOrder )
-                {
-                    n ^= 1;
-                }
-
-                result += n * square;
-                square *= 2;
-                ++order;
-            }
-        }
-
-        answer.emplace_back(result);
-    }
-
-    return answer;
-}
-
-int main()
-{
-    vector<long long> numbers = { 2, 7 };
-    vector<long long> result = solution(numbers);
-
-    for ( long long res : result )
-	    cout << res << endl;
-
-	return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv2 2개 이하로 다른 비트
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//vector<long long> solution(vector<long long> numbers)
+//{
+//    vector<long long> answer;
+//    queue<int> bitQueue;
+//
+//    for ( long long num : numbers )
+//    {
+//        long long result = 0;
+//
+//        if ( num % 2 == 0 )
+//        {
+//            result = num + 1;
+//        }
+//        else
+//        {
+//            int order = 1;
+//            int zeroPosOrder = 0;
+//            while ( true )
+//            {
+//                int remainder = num % 2;
+//                if ( remainder == 0 && zeroPosOrder == 0 )
+//                {
+//                    zeroPosOrder = order;
+//                }
+//                bitQueue.push(remainder);
+//
+//                if ( num == 0 )
+//                    break;
+//
+//                num /= 2;
+//                ++order;
+//            }
+//
+//            order = 1;
+//            long long square = 1;
+//            while ( !bitQueue.empty() )
+//            {
+//                int n = bitQueue.front();
+//                bitQueue.pop();
+//
+//                if ( order == zeroPosOrder - 1 || order == zeroPosOrder )
+//                {
+//                    n ^= 1;
+//                }
+//
+//                result += n * square;
+//                square *= 2;
+//                ++order;
+//            }
+//        }
+//
+//        answer.emplace_back(result);
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    vector<long long> numbers = { 2, 7 };
+//    vector<long long> result = solution(numbers);
+//
+//    for ( long long res : result )
+//	    cout << res << endl;
+//
+//	return 0;
+//}
