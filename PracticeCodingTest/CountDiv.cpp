599bc230e59ca7524936e05397f7b2ffc74d480e
OneBarrack <wnsdd1@gmail.com>
2022-03-25 19:59:39 +0900

    * Clear - Codility medium level - Passing cars

---
+++ PracticeCodingTest/CountDiv.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+// Codility 코딩테스트 연습 
+// Medium level - Count div
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+int solution(int A, int B, int K)
+{
+    int answer = 0;
+    int left = (A % K == 0) ? A : A + (K - (A % K));
+    int right = B - (B % K);
+
+    if ( left <= right )
+    {
+        answer = (right - left) / K + 1;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int A = 6;
+    int B = 11;
+    int K = 2;
+
+    cout << solution(A, B, K) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/PassingCars.cpp
+++ PracticeCodingTest/PassingCars.cpp
@@ -1,59 +1,59 @@
-#include <iostream>
-// Codility 코딩테스트 연습 
-// Easy level - Passing Cars
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int solution(vector<int>& A)
-{
-    if ( A.size() < 2 )
-        return 0;
-
-    int answer = 0;
-    int eastDirCount = 0;
-
-    for ( int& carDir : A )
-    {
-        if ( carDir == 0 )
-        {
-            ++eastDirCount;
-        }
-        else if ( answer + eastDirCount <= 1000000000 )
-        {
-            answer += eastDirCount;
-        }
-        else
-        {
-            answer = -1;
-            break;
-        }
-    }
-
-    return answer;
-}
-
-int main()
-{
-    vector<int> A = { 0, 1, 0, 1, 1 };
-    //vector<int> A = { 1, 2, 3 };
-    //vector<int> A = { -1, -3 };
-
-    //vector<int> result = solution(N, A);
-    //for ( int res : result )
-    //    cout << res << endl;
-
-    cout << solution(A) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// Codility 코딩테스트 연습 
+//// Easy level - Passing Cars
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int solution(vector<int>& A)
+//{
+//    if ( A.size() < 2 )
+//        return 0;
+//
+//    int answer = 0;
+//    int eastDirCount = 0;
+//
+//    for ( int& carDir : A )
+//    {
+//        if ( carDir == 0 )
+//        {
+//            ++eastDirCount;
+//        }
+//        else if ( answer + eastDirCount <= 1000000000 )
+//        {
+//            answer += eastDirCount;
+//        }
+//        else
+//        {
+//            answer = -1;
+//            break;
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    vector<int> A = { 0, 1, 0, 1, 1 };
+//    //vector<int> A = { 1, 2, 3 };
+//    //vector<int> A = { -1, -3 };
+//
+//    //vector<int> result = solution(N, A);
+//    //for ( int res : result )
+//    //    cout << res << endl;
+//
+//    cout << solution(A) << endl;
+//
+//    return 0;
+//}
