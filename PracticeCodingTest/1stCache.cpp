5676dec7ce1562130e49525e17ab264dbb38ce98
OneBarrack <wnsdd1@gmail.com>
2022-04-02 04:34:55 +0900

    * Clear - Programmers Lv1. 2016 Year

--- PracticeCodingTest/1stCache.cpp
+++ PracticeCodingTest/1stCache.cpp
@@ -1,72 +1,72 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv2 [1차] 캐시
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <numeric>
-
-using namespace std;
-
-int solution(int cacheSize, vector<string> cities)
-{
-    int answer = 0;
-
-    unordered_map<string, bool> isCached;
-    list<string> cache;
-
-    for ( string city : cities )
-    {
-        transform(city.begin(), city.end(), city.begin(), ::tolower);
-        if ( isCached[city] )
-        {
-            ++answer;
-            if ( *cache.rbegin() != city )
-            {
-                cache.erase(find(cache.begin(), cache.end(), city));
-                cache.push_back(city);
-            }
-        }
-        else
-        {
-            if ( cacheSize > 0 )
-            {
-                if ( cache.size() == cacheSize )
-                {
-                    auto cachedCityIter = find(cache.begin(), cache.end(), city);
-                    if ( cachedCityIter != cache.end() )
-                    {
-                        cache.erase(cachedCityIter);
-                    }
-                    isCached[cache.front()] = false;
-                    cache.pop_front();
-                }
-
-                cache.push_back(city);
-                isCached[city] = true;
-            }
-
-            answer += 5;
-        }
-    }
-
-    return answer;
-}
-
-int main()
-{	
-    int cacheSize = 2;
-    vector<string> cities = { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome" };
-
-	cout << solution(cacheSize, cities) << endl;
-
-	return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv2 [1차] 캐시
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//#include <numeric>
+//
+//using namespace std;
+//
+//int solution(int cacheSize, vector<string> cities)
+//{
+//    int answer = 0;
+//
+//    unordered_map<string, bool> isCached;
+//    list<string> cache;
+//
+//    for ( string city : cities )
+//    {
+//        transform(city.begin(), city.end(), city.begin(), ::tolower);
+//        if ( isCached[city] )
+//        {
+//            ++answer;
+//            if ( *cache.rbegin() != city )
+//            {
+//                cache.erase(find(cache.begin(), cache.end(), city));
+//                cache.push_back(city);
+//            }
+//        }
+//        else
+//        {
+//            if ( cacheSize > 0 )
+//            {
+//                if ( cache.size() == cacheSize )
+//                {
+//                    auto cachedCityIter = find(cache.begin(), cache.end(), city);
+//                    if ( cachedCityIter != cache.end() )
+//                    {
+//                        cache.erase(cachedCityIter);
+//                    }
+//                    isCached[cache.front()] = false;
+//                    cache.pop_front();
+//                }
+//
+//                cache.push_back(city);
+//                isCached[city] = true;
+//            }
+//
+//            answer += 5;
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{	
+//    int cacheSize = 2;
+//    vector<string> cities = { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome" };
+//
+//	cout << solution(cacheSize, cities) << endl;
+//
+//	return 0;
+//}
---
+++ PracticeCodingTest/2016Year.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv1 2016년
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+string solution(int a, int b)
+{
+    vector<string> days = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+    vector<int> months = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    int dayCount = -1;
+    for ( int i = 0; i < a - 1; i++ )
+    {
+        dayCount += months[i];
+    }
+    dayCount += b;
+
+    return days[dayCount % 7];
+}
+
+int main()
+{
+    int a = 5;
+    int b = 24;
+    cout << solution(a, b) << endl;
+
+    return 0;
+}
