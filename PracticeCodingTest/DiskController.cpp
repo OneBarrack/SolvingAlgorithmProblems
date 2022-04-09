cf4b00c03b6fb0695d70006e7744a6f913fe6677
OneBarrack <wnsdd1@gmail.com>
2022-04-10 06:43:23 +0900

    * Clear - Programmers Lv2. Vowel dictionary

--- PracticeCodingTest/DiskController.cpp
+++ PracticeCodingTest/DiskController.cpp
@@ -1,75 +1,75 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv.3 - 디스크 컨트롤러
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <numeric>
-
-using namespace std;
-
-int solution(vector<vector<int>> jobs)
-{
-    sort(jobs.begin(), jobs.end());
-
-    int totalProgressTime = 0;
-    int diskProgressTime = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    for ( vector<int> job : jobs )
-    {
-        int startTime = job[0];
-        int taskTime = job[1];
-
-        while ( startTime > diskProgressTime )
-        {
-            if ( !pq.empty() )
-            {
-                int curStartTime = pq.top().second;
-                int curTaskTime = pq.top().first;
-                pq.pop();
-
-                diskProgressTime += curTaskTime;
-                totalProgressTime += (diskProgressTime - curStartTime);
-            }
-            else
-            {
-                diskProgressTime = startTime;
-            }
-        }
-
-        pq.push(make_pair(taskTime, startTime));
-    }
-
-    while ( !pq.empty() )
-    {
-        int curStartTime = pq.top().second;
-        int curTaskTime = pq.top().first;
-        pq.pop();
-
-        diskProgressTime += curTaskTime;
-        totalProgressTime += (diskProgressTime - curStartTime);
-    }
-
-    return totalProgressTime / jobs.size();
-}
-
-int main()
-{
-    //vector<vector<int>> jobs = { {0, 10} };
-    //vector<vector<int>> jobs = { {0, 10}, {0, 10} };
-    vector<vector<int>> jobs = { {0, 5}, {6, 1}, {6, 2} };
-    //vector<vector<int>> jobs = { {0, 3}, {1, 9}, {2, 6}, {4, 3} };
-
-    cout << solution(jobs) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv.3 - 디스크 컨트롤러
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//#include <numeric>
+//
+//using namespace std;
+//
+//int solution(vector<vector<int>> jobs)
+//{
+//    sort(jobs.begin(), jobs.end());
+//
+//    int totalProgressTime = 0;
+//    int diskProgressTime = 0;
+//    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//
+//    for ( vector<int> job : jobs )
+//    {
+//        int startTime = job[0];
+//        int taskTime = job[1];
+//
+//        while ( startTime > diskProgressTime )
+//        {
+//            if ( !pq.empty() )
+//            {
+//                int curStartTime = pq.top().second;
+//                int curTaskTime = pq.top().first;
+//                pq.pop();
+//
+//                diskProgressTime += curTaskTime;
+//                totalProgressTime += (diskProgressTime - curStartTime);
+//            }
+//            else
+//            {
+//                diskProgressTime = startTime;
+//            }
+//        }
+//
+//        pq.push(make_pair(taskTime, startTime));
+//    }
+//
+//    while ( !pq.empty() )
+//    {
+//        int curStartTime = pq.top().second;
+//        int curTaskTime = pq.top().first;
+//        pq.pop();
+//
+//        diskProgressTime += curTaskTime;
+//        totalProgressTime += (diskProgressTime - curStartTime);
+//    }
+//
+//    return totalProgressTime / jobs.size();
+//}
+//
+//int main()
+//{
+//    //vector<vector<int>> jobs = { {0, 10} };
+//    //vector<vector<int>> jobs = { {0, 10}, {0, 10} };
+//    vector<vector<int>> jobs = { {0, 5}, {6, 1}, {6, 2} };
+//    //vector<vector<int>> jobs = { {0, 3}, {1, 9}, {2, 6}, {4, 3} };
+//
+//    cout << solution(jobs) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/VowelDictionary.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv.2 - 모음 사전
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+void FindWord(vector<char> v, int index, string currentWord, string targetWord, int& searchCount, bool& bFind)
+{    
+    if ( bFind )
+        return;
+
+    currentWord += v[index];
+    ++searchCount;
+    
+    if ( currentWord == targetWord )
+    {
+        bFind = true;
+    }
+    else if ( currentWord.size() < 5 )
+    {
+        for ( int i = 0; i < v.size(); ++i )
+        {
+            FindWord(v, i, currentWord, targetWord, searchCount, bFind);
+        }
+    }
+}
+
+int solution(string word)
+{
+    vector<char> v = { 'A', 'E', 'I', 'O', 'U' };
+    bool bFind = false;
+    int searchCount = 0;
+
+    for ( int i = 0; i < v.size(); ++i )
+    {
+        if ( bFind )
+            break;
+
+        FindWord(v, i, "", word, searchCount, bFind);
+    }
+
+    return searchCount;
+}
+
+int main()
+{
+    string word = "I";
+    cout << solution(word) << endl;
+
+    return 0;
+}
