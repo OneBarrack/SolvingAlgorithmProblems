5f5d2d326cce48c9a88c6db3d76e310050c6a6b4
OneBarrack <wnsdd1@gmail.com>
2022-05-29 01:28:54 +0900

    * Clear - HackerRank Lv Easy. Plus minus

--- PracticeCodingTest/Larry's Array.cpp
+++ PracticeCodingTest/Larry's Array.cpp
@@ -1,54 +1,54 @@
-#include <iostream>
-// 해커랭크 코딩테스트 연습 
-// Lv Medium. Larry's Array
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <numeric>
-
-using namespace std;
-
-string larrysArray(vector<int> A)
-{
-    for ( int i = 0; i < A.size() - 2; ++i )
-    {
-        if ( A.back() == i + 1 )
-        {
-            int temp = A[A.size() - 3];
-            A[A.size() - 3] = A[A.size() - 2];
-            A[A.size() - 2] = A[A.size() - 1];
-            A[A.size() - 1] = temp;
-        }
-
-        auto iter = find(A.begin(), A.end(), i + 1);
-        while ( A[i] > i + 1 )
-        {
-            int temp = *prev(iter);
-            *prev(iter) = *iter;
-            *iter = *next(iter);
-            *next(iter) = temp;
-            --iter;
-        }
-    }
-
-    for ( int i = 0; i < A.size(); ++i )
-        if ( A[i] > i + 1 )
-            return "NO";
-
-    return "YES";
-}
-
-int main()
-{
-    vector<int> A = { 1, 3, 4, 2 };
-    cout << larrysArray(A) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 해커랭크 코딩테스트 연습 
+//// Lv Medium. Larry's Array
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <numeric>
+//
+//using namespace std;
+//
+//string larrysArray(vector<int> A)
+//{
+//    for ( int i = 0; i < A.size() - 2; ++i )
+//    {
+//        if ( A.back() == i + 1 )
+//        {
+//            int temp = A[A.size() - 3];
+//            A[A.size() - 3] = A[A.size() - 2];
+//            A[A.size() - 2] = A[A.size() - 1];
+//            A[A.size() - 1] = temp;
+//        }
+//
+//        auto iter = find(A.begin(), A.end(), i + 1);
+//        while ( A[i] > i + 1 )
+//        {
+//            int temp = *prev(iter);
+//            *prev(iter) = *iter;
+//            *iter = *next(iter);
+//            *next(iter) = temp;
+//            --iter;
+//        }
+//    }
+//
+//    for ( int i = 0; i < A.size(); ++i )
+//        if ( A[i] > i + 1 )
+//            return "NO";
+//
+//    return "YES";
+//}
+//
+//int main()
+//{
+//    vector<int> A = { 1, 3, 4, 2 };
+//    cout << larrysArray(A) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/PlusMinus.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// 해커랭크 코딩테스트 연습 
+// Lv Easy. Plus Minus
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+
+using namespace std;
+
+void plusMinus(vector<int> arr)
+{
+    int positiveCount = 0;
+    int negativeCount = 0;
+    int zeroCount = 0;
+
+    for ( int& num : arr )
+    {
+        if ( num > 0 )
+            ++positiveCount;
+        else if ( num < 0 )
+            ++negativeCount;
+        else
+            ++zeroCount;
+    }
+
+    cout << (float)positiveCount / arr.size() << endl << (float)negativeCount / arr.size() << endl << (float)zeroCount / arr.size();
+}
+
+int main()
+{
+    vector<int> arr = { -4, 3, -9, 0, 4, 1 };
+    plusMinus(arr);
+
+    return 0;
+}
