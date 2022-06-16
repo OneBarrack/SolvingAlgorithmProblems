4c66118dc5079774f968877bcf50039ed33d8036
OneBarrack <wnsdd1@gmail.com>
2022-06-16 22:21:02 +0900

    * Clear - Baekjoon Lv Bronze2. The number of words

---
+++ PracticeCodingTest/TheNumberOfWords.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+// 백준 코딩테스트 연습 
+// Lv.Bronze2 - The Number of Words
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+/*
+The Curious Case of Benjamin Button
+*/
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    if ( s == " " )
+    {
+        cout << 0;
+    }
+    else
+    {
+        int blankCount = count(s.begin(), s.end(), ' ');
+        if ( s.front() == ' ' ) --blankCount;
+        if ( s.back() == ' ' ) --blankCount;
+        cout << blankCount + 1;
+    }
+
+    return 0;
+}
