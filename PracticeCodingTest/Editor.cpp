87a5395592b6589c789af7942fa73e6c940120c8
OneBarrack <wnsdd1@gmail.com>
2022-06-30 19:09:35 +0900

    * Clear - Baekjoon Lv Silver1. Zoo

--- PracticeCodingTest/Editor.cpp
+++ PracticeCodingTest/Editor.cpp
@@ -1,89 +1,89 @@
-#include <iostream>
-// 백준 코딩테스트 연습 
-// Lv.Silver2 - Editor
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <numeric>
-#include <deque>
-
-using namespace std;
-/*
-예제 입력 1
-abcd
-3
-P x
-L
-P y
-예제 출력 1
-abcdyx
-*/
-int main()
-{
-	string str;
-	cin >> str;
-
-	list<char> chars(str.begin(), str.end());
-	auto cursor = chars.end();
-
-	int M;
-	cin >> M;
-
-	while ( M > 0 )
-	{
-		char command;
-		cin >> command;
-
-		switch ( command )
-		{
-			case 'L':
-			{
-				if ( cursor != chars.begin() )
-					--cursor;
-
-				break;
-			}				
-			case 'D':
-			{
-				if ( cursor != chars.end() )
-					++cursor;
-
-				break;
-			}
-			case 'B':
-			{
-				if ( cursor != chars.begin() )
-					cursor = chars.erase(--cursor);
-
-				break;
-			}
-			case 'P':
-			{
-				char c;
-				cin >> c;
-
-				if ( cursor == chars.end() )
-					chars.push_back(c);
-				else
-					chars.insert(cursor, c);
-
-				break;
-			}
-		}
-
-		--M;
-	}
-
-	for ( char c : chars )
-		cout << c;
-
-	return 0;
-}
+//#include <iostream>
+//// 백준 코딩테스트 연습 
+//// Lv.Silver2 - Editor
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//#include <numeric>
+//#include <deque>
+//
+//using namespace std;
+///*
+//예제 입력 1
+//abcd
+//3
+//P x
+//L
+//P y
+//예제 출력 1
+//abcdyx
+//*/
+//int main()
+//{
+//	string str;
+//	cin >> str;
+//
+//	list<char> chars(str.begin(), str.end());
+//	auto cursor = chars.end();
+//
+//	int M;
+//	cin >> M;
+//
+//	while ( M > 0 )
+//	{
+//		char command;
+//		cin >> command;
+//
+//		switch ( command )
+//		{
+//			case 'L':
+//			{
+//				if ( cursor != chars.begin() )
+//					--cursor;
+//
+//				break;
+//			}				
+//			case 'D':
+//			{
+//				if ( cursor != chars.end() )
+//					++cursor;
+//
+//				break;
+//			}
+//			case 'B':
+//			{
+//				if ( cursor != chars.begin() )
+//					cursor = chars.erase(--cursor);
+//
+//				break;
+//			}
+//			case 'P':
+//			{
+//				char c;
+//				cin >> c;
+//
+//				if ( cursor == chars.end() )
+//					chars.push_back(c);
+//				else
+//					chars.insert(cursor, c);
+//
+//				break;
+//			}
+//		}
+//
+//		--M;
+//	}
+//
+//	for ( char c : chars )
+//		cout << c;
+//
+//	return 0;
+//}
---
+++ PracticeCodingTest/Zoo.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+// 백준 코딩테스트 연습 
+// Lv.Silver1 - Zoo
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+#include <numeric>
+#include <deque>
+
+using namespace std;
+/*
+예제 입력 1
+5 4
+3 1
+3 2
+4 3
+5 3
+예제 출력 1
+1 2
+*/
+enum class ESettingCase
+{
+	EMPTY,
+	LEFT,
+	RIGHT
+};
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	map<ESettingCase, int> m1;
+	m1[ESettingCase::EMPTY] = m1[ESettingCase::LEFT] = m1[ESettingCase::RIGHT] = 1;
+
+	for ( int i = 1; i < N; ++i )
+	{
+		map<ESettingCase, int> m2;
+		m2[ESettingCase::EMPTY] = (m1[ESettingCase::EMPTY] + m1[ESettingCase::LEFT] + m1[ESettingCase::RIGHT]) % 9901;
+		m2[ESettingCase::LEFT]  = (m1[ESettingCase::EMPTY] + m1[ESettingCase::RIGHT]) % 9901;
+		m2[ESettingCase::RIGHT] = (m1[ESettingCase::EMPTY] + m1[ESettingCase::LEFT]) % 9901;
+		m1 = m2;
+	}
+
+	cout << (m1[ESettingCase::EMPTY] + m1[ESettingCase::LEFT] + m1[ESettingCase::RIGHT]) % 9901;
+
+	return 0;
+}
