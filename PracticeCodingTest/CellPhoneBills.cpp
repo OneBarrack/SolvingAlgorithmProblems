7f009359442a5d258d71cef666865ce179235e01
OneBarrack <wnsdd1@gmail.com>
2022-07-04 01:08:37 +0900

    * Clear - Baekjoon Lv Bronze3. Call phone bills

---
+++ PracticeCodingTest/CellPhoneBills.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv.Bronze3 - Cell Phone Bills
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+/*
+예제 입력 1 
+3
+40 40 40
+예제 출력 1 
+M 45
+영식: 20 + 20 + 20 = 60
+민식: 15 + 15 + 15 = 45
+예제 입력 2 
+3
+61 61 61
+예제 출력 2 
+Y M 90
+예제 입력 3 
+2
+61 10
+예제 출력 3 
+Y 40
+*/
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> callingTimes(N);
+    for ( int i = 0; i < N; ++i )
+        cin >> callingTimes[i];
+
+    int FeeByYSPlan = 0;
+    int FeeByMSPlan = 0;
+    for ( int callingTime : callingTimes )
+    {         
+        FeeByYSPlan += 10 + callingTime / 30 * 10;
+        FeeByMSPlan += 15 + callingTime / 60 * 15;
+    }
+
+    if ( FeeByYSPlan < FeeByMSPlan )
+        cout << "Y " << FeeByYSPlan;
+    else if ( FeeByYSPlan > FeeByMSPlan )
+        cout << "M " << FeeByMSPlan;
+    else // ( FeeByYSPlan == FeeByMSPlan )
+        cout << "Y M " << FeeByYSPlan;
+
+    return 0;
+}
--- PracticeCodingTest/R2.cpp
+++ PracticeCodingTest/R2.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv.Bronze4 - R2
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-int main()
-{
-    int R1, S;
-    cin >> R1 >> S;
-    cout << S * 2 - R1;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv.Bronze4 - R2
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int R1, S;
+//    cin >> R1 >> S;
+//    cout << S * 2 - R1;
+//
+//    return 0;
+//}
