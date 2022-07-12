47f93728574fbee547d894ca46b25272a2bc994d
OneBarrack <wnsdd1@gmail.com>
2022-07-12 22:26:33 +0900

    * Clear - Baekjoon Lv Bronze1. Find faction

--- PracticeCodingTest/BasketballGame.cpp
+++ PracticeCodingTest/BasketballGame.cpp
@@ -1,84 +1,84 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv.Bronze4 - Basketball Game
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-/*
-예제 입력 1
-18
-babic
-keksic
-boric
-bukic
-sarmic
-balic
-kruzic
-hrenovkic
-beslic
-boksic
-krafnic
-pecivic
-klavirkovic
-kukumaric
-sunkic
-kolacic
-kovacic
-prijestolonasljednikovi
-예제 출력 1
-bk
-예제 입력 2
-6
-michael
-jordan
-lebron
-james
-kobe
-bryant
-예제 출력 2
-PREDAJA
-*/
-
-int main()
-{
-    int N;
-    cin >> N;
-
-    map<char, int> nameCountInfos;
-
-    while ( N > 0 )
-    {
-        string name;
-        cin >> name;
-        
-        ++nameCountInfos[name[0]];
-
-        --N;
-    }
-
-    string result = "";
-    for ( auto& nameCountInfo : nameCountInfos )
-    {
-        if ( nameCountInfo.second < 5 )
-            continue;
-
-        result += nameCountInfo.first;
-    }
-
-    if ( result.empty() )
-        cout << "PREDAJA";
-    else
-        cout << result;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv.Bronze4 - Basketball Game
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+///*
+//예제 입력 1
+//18
+//babic
+//keksic
+//boric
+//bukic
+//sarmic
+//balic
+//kruzic
+//hrenovkic
+//beslic
+//boksic
+//krafnic
+//pecivic
+//klavirkovic
+//kukumaric
+//sunkic
+//kolacic
+//kovacic
+//prijestolonasljednikovi
+//예제 출력 1
+//bk
+//예제 입력 2
+//6
+//michael
+//jordan
+//lebron
+//james
+//kobe
+//bryant
+//예제 출력 2
+//PREDAJA
+//*/
+//
+//int main()
+//{
+//    int N;
+//    cin >> N;
+//
+//    map<char, int> nameCountInfos;
+//
+//    while ( N > 0 )
+//    {
+//        string name;
+//        cin >> name;
+//        
+//        ++nameCountInfos[name[0]];
+//
+//        --N;
+//    }
+//
+//    string result = "";
+//    for ( auto& nameCountInfo : nameCountInfos )
+//    {
+//        if ( nameCountInfo.second < 5 )
+//            continue;
+//
+//        result += nameCountInfo.first;
+//    }
+//
+//    if ( result.empty() )
+//        cout << "PREDAJA";
+//    else
+//        cout << result;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/FindFraction.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv.Bronze4 - Basketball Game
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+/*
+예제 입력 1
+1
+예제 출력 1
+1/1
+예제 입력 2
+2
+예제 출력 2
+1/2
+예제 입력 3
+3
+예제 출력 3
+2/1
+예제 입력 4
+4
+예제 출력 4
+3/1
+예제 입력 5
+5
+예제 출력 5
+2/2
+예제 입력 6
+6
+예제 출력 6
+1/3
+예제 입력 7
+7
+예제 출력 7
+1/4
+예제 입력 8
+8
+예제 출력 8
+2/3
+예제 입력 9
+9
+예제 출력 9
+3/2
+예제 입력 10
+14
+예제 출력 10
+2/4
+*/
+
+int main()
+{
+    int X;
+    cin >> X;
+    
+    int n = 1;
+    while ( X > n )
+    {
+        X -= n;
+        ++n;
+    }
+    
+    if ( n & 1 )
+        cout << n + 1 - X << "/" << X;
+    else
+        cout << X << "/" << n + 1 - X;
+
+    return 0;
+}
