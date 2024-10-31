a8c54d65074a095c8b6fccf07c08a38988e12820
준우 황 <wnsdd1@gmail.com>
2024-10-31 22:22:59 +0900

    * Clear -  Programmers Lv2. Sum of consecutive subsequences

--- PracticeCodingTest/Sign.cpp
+++ PracticeCodingTest/Sign.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// Lv.Bronze3 - 1247. Sign
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// Lv.Bronze3 - 1247. Sign
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
 
 /*
 예제 입력 1
@@ -44,59 +44,59 @@ using namespace std;
 +
 965310000
 */
-int main()
-{
-    for ( int i = 0; i < 3; ++i )
-    {
-        int N;
-        cin >> N;
-
-        long long sumNum = 0;
-        int overflowCount = 0;
-
-        while ( N-- > 0 )
-        {
-            long long inputNum;
-            cin >> inputNum;
-            
-            if ( sumNum > 0 && inputNum > 0 && sumNum > LLONG_MAX - inputNum )
-            {
-                ++overflowCount;
-            }
-            else if ( sumNum < 0 && inputNum < 0 && sumNum < LLONG_MIN - inputNum )
-            {
-                --overflowCount;
-            }
-            sumNum += inputNum;
-        }
-
-        string result = "";
-        if ( overflowCount == 0 )
-        {
-            if ( sumNum == 0 )
-            {
-                result = "0";
-            }
-            else if ( sumNum > 0 )
-            {
-                result = "+";
-            }
-            else
-            {
-                result = "-";
-            }
-        }
-        else if ( overflowCount > 0 )
-        {
-            result = "+";
-        }
-        else
-        {
-            result = "-";
-        }
-
-        cout << result << '\n';
-    }
-    
-    return 0;
-}
+//int main()
+//{
+//    for ( int i = 0; i < 3; ++i )
+//    {
+//        int N;
+//        cin >> N;
+//
+//        long long sumNum = 0;
+//        int overflowCount = 0;
+//
+//        while ( N-- > 0 )
+//        {
+//            long long inputNum;
+//            cin >> inputNum;
+//            
+//            if ( sumNum > 0 && inputNum > 0 && sumNum > LLONG_MAX - inputNum )
+//            {
+//                ++overflowCount;
+//            }
+//            else if ( sumNum < 0 && inputNum < 0 && sumNum < LLONG_MIN - inputNum )
+//            {
+//                --overflowCount;
+//            }
+//            sumNum += inputNum;
+//        }
+//
+//        string result = "";
+//        if ( overflowCount == 0 )
+//        {
+//            if ( sumNum == 0 )
+//            {
+//                result = "0";
+//            }
+//            else if ( sumNum > 0 )
+//            {
+//                result = "+";
+//            }
+//            else
+//            {
+//                result = "-";
+//            }
+//        }
+//        else if ( overflowCount > 0 )
+//        {
+//            result = "+";
+//        }
+//        else
+//        {
+//            result = "-";
+//        }
+//
+//        cout << result << '\n';
+//    }
+//    
+//    return 0;
+//}
---
+++ PracticeCodingTest/SumOfConsecutiveSubsequences.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// Lv2. 연속된 부분 수열의 합
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    vector<int> answer;
+    
+    int sum = 0;
+
+    int front;
+    int back = sequence.size() - 1;
+
+    for (int i = sequence.size() - 1; i >= 0; i--)
+    {
+        front = i;
+
+        sum += sequence[front];
+
+        if (sum > k || (i > 0 && sum + sequence[front - 1] - sequence[back] == k))
+        {
+            sum -= sequence[back];
+            --back;
+        }
+        else if (sum == k)
+        {
+            answer.push_back(front);
+            answer.push_back(back);
+
+            break;
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    //vector<int> sequence = { 1, 1, 1, 2, 3, 4, 5 };
+    //int k = 5;
+
+    vector<int> sequence = { 2, 2, 2, 2, 2 };
+    int k = 6;
+
+    vector<int> result = solution(sequence, k);
+    
+    cout << result[0] << " " << result[1] << endl;
+
+    return 0;
+}
