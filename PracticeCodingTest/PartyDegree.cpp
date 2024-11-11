17d1a461ab32976bdd0e385eb256f39f487fa5e2
준우 황 <wnsdd1@gmail.com>
2024-11-11 09:23:33 +0900

    * Clear - Programmers [PCCP] No1. WrapABandage

--- PracticeCodingTest/PartyDegree.cpp
+++ PracticeCodingTest/PartyDegree.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-// 코딜리티 코딩테스트 연습 
-// Excercise 5 Coding skills easy. PartyDegree
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-int solution(int N) {
-    if (N == 1)
-        return 0;
-
-    int answer = 0;
-    while (N % 2 == 0)
-    {
-        N /= 2;
-        ++answer;
-    }
-
-    return answer;
-}
-
-int main()
-{
-    cout << solution(24) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 코딜리티 코딩테스트 연습 
+//// Excercise 5 Coding skills easy. PartyDegree
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+//int solution(int N) {
+//    if (N == 1)
+//        return 0;
+//
+//    int answer = 0;
+//    while (N % 2 == 0)
+//    {
+//        N /= 2;
+//        ++answer;
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    cout << solution(24) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/WrapABandage.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// [PCCP 기출문제] 1번 / 붕대 감기
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
+    int maxHealth = health;
+
+    int prevAttackTime = -1;
+    for (vector<int>& attack : attacks)
+    {
+        int curAttackTime = attack[0];
+        int damage = attack[1];
+        int attackTimeGap = curAttackTime - prevAttackTime;
+
+        health += ((attackTimeGap - 1) * bandage[1]) + (((attackTimeGap - 1) / bandage[0]) * bandage[2]);
+        health = min(health, maxHealth);
+
+        health -= damage;
+        prevAttackTime = curAttackTime;
+
+        if (health < 1)
+            return -1;
+    }
+
+    return health;
+}
+
+int main()
+{
+    //vector<int> bandage = {5, 1, 5}; 
+    //int health = 30; 
+    //vector<vector<int>> attacks = {{2, 10}, {9, 15}, {10, 5}, {11, 5}};
+
+    vector<int> bandage = {3, 2, 7}; 
+    int health = 20; 
+    vector<vector<int>> attacks = {{1, 15}, {5, 16}, {8, 6}};
+
+    cout << solution(bandage, health, attacks) << endl;
+
+    return 0;
+}
