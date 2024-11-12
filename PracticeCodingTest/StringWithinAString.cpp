b42a9b1b37e319fad5014fca460bb9d97d0384c9
준우 황 <wnsdd1@gmail.com>
2024-11-13 05:41:56 +0900

    * Clear - Programmers Lv1. ADeckOfCards

--- PracticeCodingTest/StringWithinAString.cpp
+++ PracticeCodingTest/StringWithinAString.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 문자열 안에 문자열
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-int solution(string str1, string str2) {
-    return (str1.find(str2) != string::npos) ? 1 : 2;
-}
-
-int main()
-{
-    string str1 = "ab6CDE443fgh22iJKlmn1o";
-    string str2 = "6CD";
-
-    cout << solution(str1, str2) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 문자열 안에 문자열
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+//int solution(string str1, string str2) {
+//    return (str1.find(str2) != string::npos) ? 1 : 2;
+//}
+//
+//int main()
+//{
+//    string str1 = "ab6CDE443fgh22iJKlmn1o";
+//    string str2 = "6CD";
+//
+//    cout << solution(str1, str2) << endl;
+//
+//    return 0;
+//}
