62aa15c4fc93981b5d9bf423c13f7efc3943bbb8
준우 황 <wnsdd1@gmail.com>
2024-11-30 20:48:26 +0900

    * Clear - Codility Lesson 15 easy. ABSDistinct

--- PracticeCodingTest/TheThreeMusketeers.cpp
+++ PracticeCodingTest/TheThreeMusketeers.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
-// Codility 코딩테스트 연습 
-// Exercise 9 easy. MaxSliceSum
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-int solution(vector<int> number) {
-    int answer = 0;
-
-    for (int i = 0; i < number.size() - 2; i++)
-    {
-        for (int j = i + 1; j < number.size() - 1; j++)
-        {
-            for (int k = j + 1; k < number.size(); k++)
-            {
-                if (number[i] + number[j] + number[k] == 0)
-                    ++answer;
-            }
-        }
-    }
-
-    return answer;
-}
-
-int main()
-{
-    vector<int> number = {-2, 3, 0, 2, -5};
-
-    cout << solution(number) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// Codility 코딩테스트 연습 
+//// Exercise 9 easy. MaxSliceSum
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+//int solution(vector<int> number) {
+//    int answer = 0;
+//
+//    for (int i = 0; i < number.size() - 2; i++)
+//    {
+//        for (int j = i + 1; j < number.size() - 1; j++)
+//        {
+//            for (int k = j + 1; k < number.size(); k++)
+//            {
+//                if (number[i] + number[j] + number[k] == 0)
+//                    ++answer;
+//            }
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    vector<int> number = {-2, 3, 0, 2, -5};
+//
+//    cout << solution(number) << endl;
+//
+//    return 0;
+//}
