d17ee076727fe6cbeb21badf2b868c2c2fceaca8
준우 황 <wnsdd1@gmail.com>
2024-12-02 19:38:14 +0900

    * Clear - Programmers Practice. RunningRace

---
+++ PracticeCodingTest/RunningRace.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+// Codility 코딩테스트 연습 
+// Lesson 10 easy. CountFactors
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+vector<string> solution(vector<string> players, vector<string> callings) {
+    unordered_map<string, int> playerRanks;
+
+    for (int i = 0; i < players.size(); i++)
+    {
+        playerRanks[players[i]] = i;
+    }
+
+    for (string& call : callings)
+    {
+        string frontPlayer = players[playerRanks[call] - 1];
+
+        swap(players[playerRanks[call]], players[playerRanks[call] - 1]);
+        playerRanks[frontPlayer]++;
+        playerRanks[call]--;
+    }
+
+    return players;
+}
+
+int main()
+{
+    vector<string> players = {"mumu", "soe", "poe", "kai", "mine"};
+    vector<string> callings = {"kai", "kai", "mine", "mine"};
+
+    vector<string> result = solution(players, callings);
+    for (string res : result)
+        cout << res << endl;
+
+    return 0;
+}
