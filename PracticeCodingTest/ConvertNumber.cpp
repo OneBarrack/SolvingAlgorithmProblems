e8af0a65d389435a7733fd78c6b03d8dc344b8f1
준우 황 <wnsdd1@gmail.com>
2024-12-07 21:32:39 +0900

    * Clear - Programmers Practice. RicochetRobot

--- PracticeCodingTest/ConvertNumber.cpp
+++ PracticeCodingTest/ConvertNumber.cpp
@@ -1,63 +1,63 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 연습문제. 숫자 변환하기
-
-#include <string>
-#include <vector>
-#include <queue>
-#include <set>
-#include <algorithm>
-#include <unordered_map>
-#include <numeric>
-#include <stack>
-#include <map>
-
-using namespace std;
-
-int solution(int x, int y, int n) {
-    if (x == y)
-        return 0;
-
-    int answer = 0;
-
-    set<int> xSet;
-    xSet.insert(x);
-
-    while (!xSet.empty())
-    {
-        set<int> tempSet;
-
-        for (int tempX : xSet)
-        {
-            if (tempX >= y)
-            {
-                if (tempX == y)
-                {
-                    return answer;
-                }
-
-                continue;
-            }
-
-            tempSet.insert(tempX + n);
-            tempSet.insert(tempX * 2);
-            tempSet.insert(tempX * 3);
-        }
-
-        xSet = tempSet;
-        ++answer;
-    }
-
-    return -1;
-}
-
-int main()
-{
-    int x = 10;
-    int y = 40;
-    int n = 5;
-
-    cout << solution(10, 40, 5) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 연습문제. 숫자 변환하기
+//
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <set>
+//#include <algorithm>
+//#include <unordered_map>
+//#include <numeric>
+//#include <stack>
+//#include <map>
+//
+//using namespace std;
+//
+//int solution(int x, int y, int n) {
+//    if (x == y)
+//        return 0;
+//
+//    int answer = 0;
+//
+//    set<int> xSet;
+//    xSet.insert(x);
+//
+//    while (!xSet.empty())
+//    {
+//        set<int> tempSet;
+//
+//        for (int tempX : xSet)
+//        {
+//            if (tempX >= y)
+//            {
+//                if (tempX == y)
+//                {
+//                    return answer;
+//                }
+//
+//                continue;
+//            }
+//
+//            tempSet.insert(tempX + n);
+//            tempSet.insert(tempX * 2);
+//            tempSet.insert(tempX * 3);
+//        }
+//
+//        xSet = tempSet;
+//        ++answer;
+//    }
+//
+//    return -1;
+//}
+//
+//int main()
+//{
+//    int x = 10;
+//    int y = 40;
+//    int n = 5;
+//
+//    cout << solution(10, 40, 5) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/RicochetRobot.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// 연습문제. 리코쳇 로봇
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int solution(vector<string> board) {
+    vector<int> sPos, ePos;
+
+    bool bFoundR = false;
+    bool bFoundG = false;
+    for (int i = 0; i < board.size(); i++)
+    {
+        if (bFoundR && bFoundG)
+            break;
+
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            if (bFoundR && bFoundG)
+                break;
+
+            if (board[i][j] == 'R') sPos = vector<int>{i, j, 0};
+            if (board[i][j] == 'G') ePos = vector<int>{i, j, 0};
+        }
+    }
+
+    bool visit[100][100] = {false,};
+
+    queue<vector<int>> q;
+    q.push(sPos);
+    visit[sPos[0]][sPos[1]] = true;
+
+    while (!q.empty())
+    {
+        vector<int> curPos = q.front();
+        q.pop();
+
+        int row = curPos[0];
+        int col = curPos[1];
+        int depth = curPos[2];
+
+        // goal
+        if (board[row][col] == 'G')
+            return depth;
+
+        // left
+        for (int i = 1; ; i++)
+        {
+            int nextCol = col - i;
+            if (nextCol < 0 || board[row][nextCol] == 'D')
+            {
+                if (!visit[row][nextCol + 1])
+                {
+                    q.push(vector<int>{row, nextCol + 1, depth + 1});
+                    visit[row][nextCol + 1] = true;
+                }
+                break;
+            }
+        }
+
+        // right
+        for (int i = 1; ; i++)
+        {
+            int nextCol = col + i;
+            if (nextCol > board[0].size() - 1 || board[row][nextCol] == 'D')
+            {
+                if (!visit[row][nextCol - 1])
+                {
+                    q.push(vector<int>{row, nextCol - 1, depth + 1});
+                    visit[row][nextCol - 1] = true;
+                }
+                break;
+            }
+        }
+
+        // top
+        for (int i = 1; ; i++)
+        {
+            int nextRow = row - i;
+            if (nextRow < 0 || board[nextRow][col] == 'D')
+            {
+                if (!visit[nextRow + 1][col])
+                {
+                    q.push(vector<int>{nextRow + 1, col, depth + 1});
+                    visit[nextRow + 1][col] = true;
+                }
+                break;
+            }
+        }
+
+        // bottom
+        for (int i = 1; ; i++)
+        {
+            int nextRow = row + i;
+            if (nextRow > board.size() - 1 || board[nextRow][col] == 'D')
+            {
+                if (!visit[nextRow - 1][col])
+                {
+                    q.push(vector<int>{nextRow - 1, col, depth + 1});
+                    visit[nextRow - 1][col] = true;
+                }
+                break;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<string> board = {"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."};
+    cout << solution(board) << endl;
+
+    return 0;
+}
