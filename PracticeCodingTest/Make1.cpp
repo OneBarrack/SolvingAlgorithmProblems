dfa73abfcd55f8cc128679149c00c82418975af6
준우 황 <wnsdd1@gmail.com>
2024-12-29 20:29:19 +0900

    * Clear - Programmers Practice. Make1

---
+++ PracticeCodingTest/Make1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// 1로 만들기
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+
+    for (int num : num_list)
+    {
+        while (num > 1)
+        {
+            num = (num % 2 == 0) ? (num / 2) : ((num - 1) / 2);
+            ++answer;
+        }
+    }
+
+    return answer;
+}
+
+int main(void) {
+    vector<int> num_list = {12, 4, 15, 1, 14};
+    
+    cout << solution(num_list) << endl;    
+    
+    return 0;
+}
--- PracticeCodingTest/PasteStringsAndPrint.cpp
+++ PracticeCodingTest/PasteStringsAndPrint.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 문자열 붙이고 출력하기
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int main(void) {
-    string str1, str2;
-    cin >> str1 >> str2;
-
-    cout << str1 + str2;
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 문자열 붙이고 출력하기
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int main(void) {
+//    string str1, str2;
+//    cin >> str1 >> str2;
+//
+//    cout << str1 + str2;
+//    return 0;
+//}
