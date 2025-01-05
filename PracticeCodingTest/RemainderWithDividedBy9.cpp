5421dabde0986cdfa0171f2852fc903f589765a6
준우 황 <wnsdd1@gmail.com>
2025-01-05 17:20:56 +0900

    * Clear - Programmers Practice. nCharactersAfterString.

--- PracticeCodingTest/RemainderWithDividedBy9.cpp
+++ PracticeCodingTest/RemainderWithDividedBy9.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 9로 나눈 나머지
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int solution(string number) {
-    int answer = 0;
-
-    for (char& num : number)
-    {
-        answer += num - '0';
-    }
-
-    return answer % 9;
-}
-
-int main(void) {
-    string number = "78720646226947352489";
-
-    cout << solution(number) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 9로 나눈 나머지
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int solution(string number) {
+//    int answer = 0;
+//
+//    for (char& num : number)
+//    {
+//        answer += num - '0';
+//    }
+//
+//    return answer % 9;
+//}
+//
+//int main(void) {
+//    string number = "78720646226947352489";
+//
+//    cout << solution(number) << endl;
+//
+//    return 0;
+//}
