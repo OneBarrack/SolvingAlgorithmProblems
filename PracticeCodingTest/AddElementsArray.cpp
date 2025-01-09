ed094eff07106c646334842fa3cc1a6e59298059
준우 황 <wnsdd1@gmail.com>
2025-01-09 19:35:18 +0900

    * Clear - Programmers Practice. AddElementsArray

---
+++ PracticeCodingTest/AddElementsArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// 배열의 요소만큼 추가하기
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+
+    for (int& num : arr)
+    {
+        answer.insert(answer.end(), num, num);
+    }
+
+    return answer;
+}
+
+int main(void) {
+    vector<int> arr = {5, 1, 4};
+
+    vector<int> result = solution(arr);
+    for (int& res : result)
+        cout << res << endl;
+
+    return 0;
+}
--- PracticeCodingTest/RemoveAd.cpp
+++ PracticeCodingTest/RemoveAd.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// ad 제거하기.
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-vector<string> solution(vector<string> strArr) {
-    vector<string> answer;
-
-    for (string& str : strArr)
-    {
-        if (str.find("ad") == string::npos)
-        {
-            answer.push_back(str);
-        }
-    }
-
-    return answer;
-}
-
-int main(void) {
-    vector<string> strArr = {"and", "notad", "abcd"};
-    vector<string> result = solution(strArr);
-
-    for (string& res : result)
-        cout << res << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// ad 제거하기.
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//vector<string> solution(vector<string> strArr) {
+//    vector<string> answer;
+//
+//    for (string& str : strArr)
+//    {
+//        if (str.find("ad") == string::npos)
+//        {
+//            answer.push_back(str);
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main(void) {
+//    vector<string> strArr = {"and", "notad", "abcd"};
+//    vector<string> result = solution(strArr);
+//
+//    for (string& res : result)
+//        cout << res << endl;
+//
+//    return 0;
+//}
