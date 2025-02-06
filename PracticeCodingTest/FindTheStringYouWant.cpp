031c6c262f60c98b0df652162684db450f2b7561
준우 황 <wnsdd1@gmail.com>
2025-02-06 15:40:54 +0900

    * Clear - Programmers Practice. MakeItL

--- PracticeCodingTest/FindTheStringYouWant.cpp
+++ PracticeCodingTest/FindTheStringYouWant.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 원하는 문자열 찾기
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int solution(string myString, string pat) {
-    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
-    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
-
-    return myString.find(pat) != string::npos;
-}
-
-int main(void) {
-    string myString = "AbCdEfG";
-    string pat = "aBc";
-
-    cout << solution(myString, pat) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 원하는 문자열 찾기
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int solution(string myString, string pat) {
+//    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+//    transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
+//
+//    return myString.find(pat) != string::npos;
+//}
+//
+//int main(void) {
+//    string myString = "AbCdEfG";
+//    string pat = "aBc";
+//
+//    cout << solution(myString, pat) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/MakeItL.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+// 프로그래머스 코딩테스트 연습 
+// l로 만들기
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+string solution(string myString) {
+    for (char& c : myString)
+    {
+        if (c < 'l')
+        {
+            c = 'l';
+        }
+    }
+
+    return myString;
+}
+
+int main(void) {
+    string myString = "abcdevwxyz";
+
+    cout << solution(myString) << endl;
+
+    return 0;
+}
