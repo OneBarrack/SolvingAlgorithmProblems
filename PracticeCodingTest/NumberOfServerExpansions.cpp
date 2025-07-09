5ba13371c9deb4957d7aca740a411af3a73c4937
준우 황 <wnsdd1@gmail.com>
2025-07-10 00:47:13 +0900

    * Clear - Programmers. ThePerfectCrime

--- PracticeCodingTest/NumberOfServerExpansions.cpp
+++ PracticeCodingTest/NumberOfServerExpansions.cpp
@@ -1,65 +1,65 @@
-#include <iostream>
-// 프로그래머스 코딩테스트 연습 
-// 2025 코드챌린지 2차 예선 Lv2. 서버 증설 횟수
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int solution(vector<int> players, int m, int k) {
-    int answer = 0;
-
-    queue<int> expandedServers;
-    for (int time = 0; time < players.size(); ++time)
-    {
-        while (!expandedServers.empty() && expandedServers.front() + k - 1 < time)
-        {
-            expandedServers.pop();
-        }
-
-        const int userCount = players[time];
-        const int serverCount = 1 + expandedServers.size();
-        const int allowedUserCount = serverCount * m - 1;
-
-        if (allowedUserCount < userCount)
-        {
-            int nonConnectedUserCount = userCount - allowedUserCount;
-            while (nonConnectedUserCount > 0)
-            {
-                expandedServers.push(time);
-
-                nonConnectedUserCount -= m;
-                ++answer;
-            }
-        }
-    }
-
-    return answer;
-}
-
-int main(void) {
-    vector<int> players = {0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5};
-    int m = 3;
-    int k = 5;
-
-    //vector<int> players = {0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0};
-    //int m = 5;
-    //int k = 1;
-
-    //vector<int> players = {0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
-    //int m = 1;
-    //int k = 1;
-
-    cout << solution(players, m, k) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 코딩테스트 연습 
+//// 2025 코드챌린지 2차 예선 Lv2. 서버 증설 횟수
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int solution(vector<int> players, int m, int k) {
+//    int answer = 0;
+//
+//    queue<int> expandedServers;
+//    for (int time = 0; time < players.size(); ++time)
+//    {
+//        while (!expandedServers.empty() && expandedServers.front() + k - 1 < time)
+//        {
+//            expandedServers.pop();
+//        }
+//
+//        const int userCount = players[time];
+//        const int serverCount = 1 + expandedServers.size();
+//        const int allowedUserCount = serverCount * m - 1;
+//
+//        if (allowedUserCount < userCount)
+//        {
+//            int nonConnectedUserCount = userCount - allowedUserCount;
+//            while (nonConnectedUserCount > 0)
+//            {
+//                expandedServers.push(time);
+//
+//                nonConnectedUserCount -= m;
+//                ++answer;
+//            }
+//        }
+//    }
+//
+//    return answer;
+//}
+//
+//int main(void) {
+//    vector<int> players = {0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5};
+//    int m = 3;
+//    int k = 5;
+//
+//    //vector<int> players = {0, 0, 0, 10, 0, 12, 0, 15, 0, 1, 0, 1, 0, 0, 0, 5, 0, 0, 11, 0, 8, 0, 0, 0};
+//    //int m = 5;
+//    //int k = 1;
+//
+//    //vector<int> players = {0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 5, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1};
+//    //int m = 1;
+//    //int k = 1;
+//
+//    cout << solution(players, m, k) << endl;
+//
+//    return 0;
+//}
---
+++ PracticeCodingTest/ThePerfectCrime.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+// 프로그래머스 코드챌린지 2차 예선 : 완전범죄
+// 2
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+void DFS(vector<vector<int>>& info, vector<vector<int>>& sumInfo, int n, int m, int index, int sumA, int sumB, int& answer)
+{
+    if (index >= info.size())
+    {
+        answer = min(sumA, answer);
+        return;
+    }
+
+    if (answer <= sumA)
+        return;
+
+    if (sumInfo[index][0] > -1 && sumInfo[index][1] > -1)
+    {
+        if (sumInfo[index - 1][0] == sumA && sumInfo[index - 1][1] == sumB)
+        {
+            return;
+        }
+    }
+
+    if (sumA + info[index][0] < n)
+    {
+        sumInfo[index][0] = sumA + info[index][0];
+        DFS(info, sumInfo, n, m, index + 1, sumA + info[index][0], sumB, answer);
+    }
+
+    if (sumB + info[index][1] < m)
+    {
+        sumInfo[index][1] = sumB + info[index][1];
+        DFS(info, sumInfo, n, m, index + 1, sumA, sumB + info[index][1], answer);
+    }
+}
+
+int solution(vector<vector<int>> info, int n, int m) {
+    int answer = INT_MAX;
+    vector<vector<int>> sumInfo(info.size(), vector<int>(2, -1));
+
+    sort(info.begin(), info.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+        });
+
+    DFS(info, sumInfo, n, m, 0, 0, 0, answer);
+
+    return (answer == INT_MAX) ? -1 : answer;
+}
+
+int main(void) {
+    vector<vector<int>> info = {{1, 3}, {1, 3}, {1, 1}, {1, 1}, {1, 1}, {2, 3}, {2, 3}};
+    int n = 6;
+    int m = 7;
+
+    cout << solution(info, n, m) << endl;
+
+    return 0;
+}
