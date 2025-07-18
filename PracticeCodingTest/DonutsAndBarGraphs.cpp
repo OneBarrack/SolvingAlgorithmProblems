e934b7406b6619b2a8e9598b425d66a4d209c344
준우 황 <wnsdd1@gmail.com>
2025-07-18 15:16:13 +0900

    * Clear - Programmers. DonutsAndBarGraphs

---
+++ PracticeCodingTest/DonutsAndBarGraphs.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+// 프로그래머스 2024 KAKAO WINTER INTERNSHIP : 도넛과 막대 그래프
+// 2
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+enum EShapeType
+{
+    None = 0,
+    Donut,
+    Stick,
+    Figure8
+};
+
+void Search(map<int, vector<int>>& nodes, map<int, bool>& visitInfo, int currentNode, int& cycleCount)
+{
+    if (visitInfo[currentNode])
+    {
+        cycleCount++;
+        return;
+    }
+
+    visitInfo[currentNode] = true;
+
+    for (int nextNode : nodes[currentNode])
+    {
+        Search(nodes, visitInfo, nextNode, cycleCount);
+    }
+}
+
+vector<int> solution(vector<vector<int>> edges) {
+    vector<int> answer(4);    
+    map<int, vector<int>> nodes;
+
+    map<int, int> edgeCountsOfStartNodes;
+    set<int> nonOnlyStartNodes;
+    for (vector<int>& edge : edges)
+    {
+        int startNode = edge[0];
+        int endNode = edge[1];
+
+        nodes[startNode].push_back(endNode);
+
+        // 시작 노드와 간선 수 정보 처리
+        if (nonOnlyStartNodes.find(startNode) == nonOnlyStartNodes.end())
+        {
+            edgeCountsOfStartNodes[startNode]++;
+        }
+
+        if (edgeCountsOfStartNodes.find(endNode) != edgeCountsOfStartNodes.end())
+        {
+            edgeCountsOfStartNodes.erase(endNode);
+        }
+
+        nonOnlyStartNodes.insert(endNode);
+    }
+
+    // 생성된 시작 노드 적재
+    for (auto& startNode : edgeCountsOfStartNodes)
+    {
+        if (startNode.second > 1)
+        {
+            answer[EShapeType::None] = startNode.first;
+        }
+    }
+
+    // 시작 노드로부터 탐색
+    for (int node : nodes[answer[EShapeType::None]])
+    {
+        map<int, bool> visitInfo;
+        int cycleCount = 0;
+
+        Search(nodes, visitInfo, node, cycleCount);
+
+        switch (cycleCount)
+        {
+        case 0: answer[EShapeType::Stick]++;   break;
+        case 1: answer[EShapeType::Donut]++;   break;
+        case 2: answer[EShapeType::Figure8]++; break;
+        default:
+            break;
+        }
+    }
+
+    return answer;
+}
+
+int main(void) {
+    vector<vector<int>> edges = {{2, 3} ,{4, 3}, {1, 1}, {2, 1}};
+    //vector<vector<int>> edges = {{4, 11} ,{1, 12}, {8, 3}, {12, 7}, {4, 2}, {7, 11}, {4, 8}, {9, 6}, {10, 11}, {6, 10}, {3, 5}, {11, 1}, {5, 3}, {11, 9}, {3, 8}};
+
+    vector<int> result = solution(edges);
+    for (int res : result)
+        cout << res << endl;
+    //cout << solution(edges) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/HotelReservation.cpp
+++ PracticeCodingTest/HotelReservation.cpp
@@ -1,53 +1,53 @@
-#include <iostream>
-// 프로그래머스 연습문제 / 호텔 대실
-// 2
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-
-using namespace std;
-
-int ConvertStringTimeToMin(string strTime)
-{
-    return stoi(strTime.substr(0, 2)) * 60 + stoi(strTime.substr(3, 5));
-}
-
-int solution(vector<vector<string>> book_time) {
-    int answer = 0;
-    constexpr int cleaningRoomTime = 10;
-
-    priority_queue<int, vector<int>, greater<int>> roomQueue;
-    sort(book_time.begin(), book_time.end());
-
-    for (vector<string>& timeInfo : book_time)
-    {
-        const int sTime = ConvertStringTimeToMin(timeInfo[0]);
-        const int eTime = ConvertStringTimeToMin(timeInfo[1]);
-
-        if (!roomQueue.empty() && roomQueue.top() <= sTime)
-        {
-            roomQueue.pop();
-        }
-
-        roomQueue.push(eTime + cleaningRoomTime);
-        answer = max(answer, static_cast<int>(roomQueue.size()));
-    }
-
-    return answer;
-}
-
-int main(void) {
-    vector<vector<string>> book_time = {{"15:00", "17:00"} ,{"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
-
-    cout << solution(book_time) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 연습문제 / 호텔 대실
+//// 2
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//
+//using namespace std;
+//
+//int ConvertStringTimeToMin(string strTime)
+//{
+//    return stoi(strTime.substr(0, 2)) * 60 + stoi(strTime.substr(3, 5));
+//}
+//
+//int solution(vector<vector<string>> book_time) {
+//    int answer = 0;
+//    constexpr int cleaningRoomTime = 10;
+//
+//    priority_queue<int, vector<int>, greater<int>> roomQueue;
+//    sort(book_time.begin(), book_time.end());
+//
+//    for (vector<string>& timeInfo : book_time)
+//    {
+//        const int sTime = ConvertStringTimeToMin(timeInfo[0]);
+//        const int eTime = ConvertStringTimeToMin(timeInfo[1]);
+//
+//        if (!roomQueue.empty() && roomQueue.top() <= sTime)
+//        {
+//            roomQueue.pop();
+//        }
+//
+//        roomQueue.push(eTime + cleaningRoomTime);
+//        answer = max(answer, static_cast<int>(roomQueue.size()));
+//    }
+//
+//    return answer;
+//}
+//
+//int main(void) {
+//    vector<vector<string>> book_time = {{"15:00", "17:00"} ,{"16:40", "18:20"}, {"14:20", "15:20"}, {"14:10", "19:20"}, {"18:20", "21:20"}};
+//
+//    cout << solution(book_time) << endl;
+//
+//    return 0;
+//}
