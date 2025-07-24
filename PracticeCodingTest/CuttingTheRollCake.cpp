af4f0030a630573d1c7e3b2e25090efbf5bd627f
준우 황 <wnsdd1@gmail.com>
2025-07-25 01:21:43 +0900

    * Clear - Programmers. CuttingTheRollCake

---
+++ PracticeCodingTest/CuttingTheRollCake.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+// 프로그래머스 연습문제 : 롤케이크 자르기
+// 2
+
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    map<int, int> leftPiece, rightPiece;
+    
+    leftPiece[topping[0]] = 1;
+    for (int i = 1; i < topping.size(); i++)
+        rightPiece[topping[i]]++;
+
+    int answer = leftPiece.size() == rightPiece.size() ? 1 : 0;
+    for (int i = 1; i < topping.size(); i++)
+    {
+        leftPiece[topping[i]]++;
+        rightPiece[topping[i]]--;
+
+        if (rightPiece[topping[i]] == 0)
+        {
+            rightPiece.erase(topping[i]);
+        }
+
+        if (leftPiece.size() == rightPiece.size())
+        {
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+int main(void) {
+    vector<int> topping = {1, 2, 1, 3, 1, 4, 1, 2};
+    //vector<int> topping = {1, 1, 1};
+
+    cout << solution(topping) << endl;
+
+    return 0;
+}
--- PracticeCodingTest/DefenseGame.cpp
+++ PracticeCodingTest/DefenseGame.cpp
@@ -1,75 +1,75 @@
-#include <iostream>
-// 프로그래머스 연습문제 : 디펜스 게임
-// 2
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <numeric>
-
-using namespace std;
-
-int solution(int n, int k, vector<int> enemy) {
-    multiset<int, greater<int>> defendedEnemies;
-    
-    int soldierCount = n;
-    int round = 0;
-    while (round < enemy.size())
-    {
-        const int enemyCount = enemy[round];
-
-        if (enemyCount <= soldierCount)
-        {
-            defendedEnemies.insert(enemyCount);
-            soldierCount -= enemyCount;
-        }
-        else if (k > 0)
-        {
-            if (!defendedEnemies.empty() && *defendedEnemies.begin() > enemyCount)
-            {
-                soldierCount += *defendedEnemies.begin() - enemyCount;
-                defendedEnemies.erase(defendedEnemies.begin());
-                defendedEnemies.insert(enemyCount);
-            }
-
-            k--;
-        }
-        else
-        {
-            break;
-        }
-
-        round++;
-    }
-
-    return round;
-}
-
-int main(void) {
-    //int n = 7;
-    //int k = 3;
-    //vector<int> enemy = {4, 2, 4, 5, 3, 3, 1};
-
-    //int n = 2;
-    //int k = 4;
-    //vector<int> enemy = {3, 3, 3, 3};
-
-    int n = 5;
-    int k = 1;
-    vector<int> enemy = {3, 1, 1, 1, 2, 2, 10};
-
-    //vector<int> result = solution(begin, end);
-    //for (int res : result)
-    //    cout << res << endl;
-
-    cout << solution(n, k, enemy) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 연습문제 : 디펜스 게임
+//// 2
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//#include <numeric>
+//
+//using namespace std;
+//
+//int solution(int n, int k, vector<int> enemy) {
+//    multiset<int, greater<int>> defendedEnemies;
+//    
+//    int soldierCount = n;
+//    int round = 0;
+//    while (round < enemy.size())
+//    {
+//        const int enemyCount = enemy[round];
+//
+//        if (enemyCount <= soldierCount)
+//        {
+//            defendedEnemies.insert(enemyCount);
+//            soldierCount -= enemyCount;
+//        }
+//        else if (k > 0)
+//        {
+//            if (!defendedEnemies.empty() && *defendedEnemies.begin() > enemyCount)
+//            {
+//                soldierCount += *defendedEnemies.begin() - enemyCount;
+//                defendedEnemies.erase(defendedEnemies.begin());
+//                defendedEnemies.insert(enemyCount);
+//            }
+//
+//            k--;
+//        }
+//        else
+//        {
+//            break;
+//        }
+//
+//        round++;
+//    }
+//
+//    return round;
+//}
+//
+//int main(void) {
+//    //int n = 7;
+//    //int k = 3;
+//    //vector<int> enemy = {4, 2, 4, 5, 3, 3, 1};
+//
+//    //int n = 2;
+//    //int k = 4;
+//    //vector<int> enemy = {3, 3, 3, 3};
+//
+//    int n = 5;
+//    int k = 1;
+//    vector<int> enemy = {3, 1, 1, 1, 2, 2, 10};
+//
+//    //vector<int> result = solution(begin, end);
+//    //for (int res : result)
+//    //    cout << res << endl;
+//
+//    cout << solution(n, k, enemy) << endl;
+//
+//    return 0;
+//}
