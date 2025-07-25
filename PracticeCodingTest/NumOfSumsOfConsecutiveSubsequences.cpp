c3094321422cb30325d9823d638bfb090d553581
준우 황 <wnsdd1@gmail.com>
2025-07-26 01:04:43 +0900

    * Clear - Programmers. TableHashFunction

--- PracticeCodingTest/NumOfSumsOfConsecutiveSubsequences.cpp
+++ PracticeCodingTest/NumOfSumsOfConsecutiveSubsequences.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-// 프로그래머스 연습문제 : 연속 부분 수열 합의 개수
-// 2
-
-#include <string>
-#include <list>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <numeric>
-
-using namespace std;
-
-int solution(vector<int> elements) {
-    set<int> results;
-
-    for (int pivot = 0; pivot < elements.size(); pivot++)
-    {
-        int sum = elements[pivot];
-        results.insert(sum);
-        
-        // right
-        for (int r = pivot + 1; r < elements.size(); r++)
-        {
-            sum += elements[r];
-            results.insert(sum);
-        }
-
-        // left
-        for (int l = 0; l < pivot - 1; l++)
-        {
-            sum += elements[l];
-            results.insert(sum);
-        }
-    }
-
-    return results.size();
-}
-
-int main(void) {
-    vector<int> elements = {7, 9, 1, 1, 4};
-
-    cout << solution(elements) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//// 프로그래머스 연습문제 : 연속 부분 수열 합의 개수
+//// 2
+//
+//#include <string>
+//#include <list>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//#include <stack>
+//#include <set>
+//#include <map>
+//#include <unordered_map>
+//#include <sstream>
+//#include <numeric>
+//
+//using namespace std;
+//
+//int solution(vector<int> elements) {
+//    set<int> results;
+//
+//    for (int pivot = 0; pivot < elements.size(); pivot++)
+//    {
+//        int sum = elements[pivot];
+//        results.insert(sum);
+//        
+//        // right
+//        for (int r = pivot + 1; r < elements.size(); r++)
+//        {
+//            sum += elements[r];
+//            results.insert(sum);
+//        }
+//
+//        // left
+//        for (int l = 0; l < pivot - 1; l++)
+//        {
+//            sum += elements[l];
+//            results.insert(sum);
+//        }
+//    }
+//
+//    return results.size();
+//}
+//
+//int main(void) {
+//    vector<int> elements = {7, 9, 1, 1, 4};
+//
+//    cout << solution(elements) << endl;
+//
+//    return 0;
+//}
